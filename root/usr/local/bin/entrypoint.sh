362bcf52f70b7c00ff1fe82f7f66aac315343c83
Brandon <brandon@tendency.me>
2019-08-23 21:51:05 -0500

    Bump

--- root/usr/local/bin/entrypoint.sh
+++ root/usr/local/bin/entrypoint.sh
@@ -5,20 +5,19 @@ set -euo pipefail
 if [ ! -r /data/.config/mopidy.conf ]; then
     mkdir -p /data/.config
     cp /defaults/mopidy.conf /data/.config
-    chown -R mopidy:mopidy /data
 fi
 
 if [ ! -r /data/.config/icecast.xml ]; then
     mkdir -p /data/.config
     cp /defaults/icecast.xml /data/.config
-    chown -R mopidy:mopidy /data
 fi
 
 if [ ! -r /data/logs ]; then
     mkdir -p /data/logs/mopidy
     mkdir -p /data/logs/icecast
-    chown -R mopidy:mopidy /data
 fi
+
+chown -R mopidy:mopidy /data
 
 su-exec mopidy mopidy --config /data/.config/mopidy.conf local scan
 exec "${@}"
