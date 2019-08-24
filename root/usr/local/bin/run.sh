1a62fb4204c3c42dacd7421e6917db6a15f01cac
Brandon <brandon@tendency.me>
2019-08-23 20:51:43 -0500

    Bump

--- root/usr/local/bin/run.sh
+++ root/usr/local/bin/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 trap "kill $PID" HUP INT TERM
-icecast -c /data/.config/icecast.xml -b &
+su-exec mopidy icecast -c /data/.config/icecast.xml -b &
 su-exec mopidy mopidy --config /data/.config/mopidy.conf "${@}" &
 PID=$!
 wait $PID
