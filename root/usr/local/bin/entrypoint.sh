736834824c52dba0080f912b7855e6afbdcd2c2a
Brandon <brandon@tendency.me>
2019-08-24 03:37:50 -0500

    Bump

--- root/usr/local/bin/entrypoint.sh
+++ root/usr/local/bin/entrypoint.sh
@@ -2,28 +2,29 @@
 
 set -euo pipefail
 
-if [ ! -r /data/.config/mopidy.conf ]; then
+if [ ! -r /data/.config ]; then
     mkdir -p /data/.config
+fi
+
+if [ ! -r /data/.config/mopidy.conf ]; then
     cp /defaults/mopidy.conf /data/.config
 fi
 
 if [ ! -r /data/.config/icecast.xml ]; then
-    mkdir -p /data/.config
     cp /defaults/icecast.xml /data/.config
 fi
 
-if [ ! -r /data/icecast ]; then
-    mkdir -p /data/icecast/web /data/icecast/logs /data/icecast/admin
+if [ ! -r /data/icecast/web ] || [ ! -r /data/icecast/admin ]; then
+    mkdir -p /data/icecast/web /data/icecast/admin /data/icecast/logs
     cp -r /usr/share/icecast /data/icecast
     cp /defaults/silence.mp3 /data/icecast/web/silence.mp3
 fi
 
 if [ ! -r /tmp/snapfifo ]; then
     touch /tmp/snapfifo
-    chown mopidy:mopidy /tmp/snapfifo
 fi
 
-chown -R mopidy:mopidy /data
+chown -R mopidy:mopidy /data /tmp/snapfifo
 
 su-exec mopidy mopidy --config /data/.config/mopidy.conf local scan
 exec "${@}"
